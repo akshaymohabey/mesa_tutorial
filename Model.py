05834b3378ed4f76b2f15df99b767ac7abcd3e9f
Akshay Mohabey <akshay.mohabey@gmail.com>
2024-06-29 02:43:14 +0530

    "Session 3"

--- Model.py
+++ Model.py
@@ -1,2 +1,52 @@
-#import mesa agent based modelling tutorial
+# Import Dependencies
 import mesa
+
+# Resource Classes
+
+class Sugar(mesa.Agent):
+    '''
+    Sugar:
+    - Contains an amount of Sugar
+    - Grows one amount of Sugar at each turn
+    '''
+
+    def __init__(self):
+        print("I am Sugar")
+
+class Spice(mesa.Agent):
+    '''
+    Spice:
+    - Contains an amount of Spice
+    - Grows one amount of Spice at each turn    
+    '''
+    
+    def __init__(self):
+        print("I am Spice")
+
+# Trader Class
+
+class Trader(mesa.Agent):
+    '''
+    Trader
+    - has a metabolism for Sugar and Spice
+    - harvest and trades Sugar and Spice to survive and thrive
+    '''
+
+    def __init__(self):
+        print("I am Trader")
+
+# Model Class
+
+class SugarscapeG1mt(mesa.Model):
+    '''
+    A model class to manage Sugarscape with Traders (G1mt)
+    from Growing Artificial Societies
+    '''
+
+    def __init__(self):
+        self.sugar = Sugar()
+        self.spice = Spice()
+        self.trader = Trader()
+
+
+model = SugarscapeG1mt()
